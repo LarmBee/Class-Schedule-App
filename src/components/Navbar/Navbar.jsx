5f92b8e43f1d3fdc8e33aaf156e6acde494fe3da
MissDine <enidnyatichi1212@gmail.com>
2022-08-23 10:59:48 +0300

    Feature edit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,14 +9,12 @@ function NavBar() {
   return (
       <Navbar className='navba' collapseOnSelect expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#logo">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as = {Link} to = {"/home"}>Home</Nav.Link>
-            <Nav.Link as = {Link} to = {"/chaplaincy"}>Chaplaincy</Nav.Link>
-            <Nav.Link as = {Link} to = {"/contact"}>Contact Us</Nav.Link>
-            <Nav.Link as = {Link} to = {"/support"}>Support St Paul's</Nav.Link>
+            <Nav.Link className="link" as = {Link} to = {"/home"}>Home</Nav.Link>
+            <Nav.Link className="link" as = {Link} to = {"/student"}>Student</Nav.Link>
+            <Nav.Link className="link" as = {Link} to = {"/admin"}>Admin</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
