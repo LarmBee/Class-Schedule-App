7e26edf6a8456ab1ca5bdd6b10c3e1a6b2372559
MissDine <enidnyatichi1212@gmail.com>
2022-08-24 23:25:14 +0300

    Style landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -36,8 +36,8 @@ function Landing() {
 =======
   return (
     <>
-      <div className="land mx-auto">
-      <Card className="typed text-center mx-auto" style={{ height: "70vh", width: "40vw" }}>
+      <div className="land mx-auto" >
+      <Card className="typed text-center mx-auto" style={{ height: "70vh", width: "40vw", borderRadius: "15px" }}>
         <Typed strings={["Ratiba"]} typeSpeed={150} backSpeed={150} loop />
         <h4 style={{ margin: "15px" }}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
