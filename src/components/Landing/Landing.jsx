d64796601917a2b6a38dbfee2cea7a9b47cd5f02
Brandon <brandonkanute@gmail.com>
2022-08-25 02:39:42 +0300

    changes to Landing by BrandonBranch

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import Typed from "react-typed";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import "../Landing/Landing.css";
+import mainimg from "../../images/schedule-main.jpg"
 
 function Landing() {
-  return (<>
-    <Card className="typed " style={{height: "70vh", width: "40vw"}}>
-        <Typed
-          strings={[
-            "Ratiba"
-          ]}
-          typeSpeed={150}
-          backSpeed={150}
-          loop
-        />
-        <h4 style={{margin:"15px"}}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, consequatur.
-        </h4>
-        <p style={{fontSize: "20px",margin:"10px"}}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, dolores.
-        </p>
-      </Card>
-    </>
-  );
+	return (
+		<>
+			<Card className="typed " style={{ height: "100%", width: "100%" }}>
+      <div className="design-intro"></div>
+      <img className="main-img"src={mainimg}/>
+				<h1 className="typed-intro" style={{ fontSize: "32px" }}>
+					Ratiba was built for :
+					<Typed
+						style={{ color: "#111F4D" }}
+						strings={["Simplicity", "Ease of Access"]}
+						typeSpeed={150}
+						backSpeed={150}
+						loop
+					/>
+				</h1>
+				<h4 className="main-text">
+					Start working with  <b>Ratiba.</b> and have all your class schedules in a single application
+				</h4>
+				<button className="btn btn-primary">Get Started</button>
+			</Card>
+		</>
+	);
 }
 
 export default Landing;
