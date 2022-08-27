208c9566b82b0badb2e3aff6dc28d506c7afc16e
Brandon <brandonkanute@gmail.com>
2022-08-28 02:54:15 +0300

    fetched data

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -1,90 +1,66 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import "./student.css"
+import React, { useEffect, useState } from "react";
+import "./student.css";
 
 function Student() {
-  return (
-    <div className="Cont">
-      <div className="container">
-      {/* <div className="row">
-          <div className="row">
-            <div className="user d-flex">
-            <div className="col-md-10">
-            <h1>
-                Hello <span>User</span>
-              </h1>
-            </div>
-              <div className="avatar col-md-2">
-                <img
-                  src={Photo}
-                  alt="Photo"
-                  style={{ height: "12vh", borderRadius: "50%" }}
-                />
-                <a>Edit</a>
-              </div>
-            </div>
-          </div>
-        
-      </div> */}
-      </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-sm-12">
-              <div className="card-announce">
-                <h1>Announcements</h1>
-              </div>
-            </div>
-            <div className="col-md-8">
-            <div className="row search my-2">
-              <Form className=" search d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </div>
-            <div className="row p-5 ">
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Stand Up</h2>
-                <h4>Friday Feedback Session</h4>
-                <p>5, March 2022</p>
-              </div>
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Title</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>5, March 2022</p>
-              </div>
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Title</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>5, March 2022</p>
-              </div>
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Title</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>5, March 2022</p>
-              </div>
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Title</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>5, March 2022</p>
-              </div>
-              <div className="card col-md-3 col-sm-12 m-2">
-                <h2>Title</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <p>5, March 2022</p>
-              </div>
-            </div>
-            </div>
-            
-          </div>
-        </div>
-    </div>
-  );
+	const [notes, setNotes] = useState([]);
+  const [announcements, setAnnouncements] = useState([]);
+
+  const getAnnouncements = async ()=>{
+    const response = await fetch("https://ratibar.herokuapp.com/announcements");
+		const data = await response.json();
+		setAnnouncements(data);
+		console.log(data);
+  };
+  useEffect(() => {
+		getAnnouncements();
+	}, []);
+
+	const getNotes = async () => {
+		const response = await fetch("https://ratibar.herokuapp.com/schedules");
+		const data = await response.json();
+		setNotes(data);
+		console.log(data);
+	};
+
+	useEffect(() => {
+		getNotes();
+	}, []);
+	return (
+		<div className="main-container">
+
+    {/* card of individual events */}
+			<div className="cards-container">
+				{notes.map((note) => {
+					return (
+						<div className="schedules">
+							<div className="schedule-card">
+								<h2>{note.title}</h2>
+								<h4>{note.description}</h4>
+								<a href={note.meeting_link}>{note.meeting_link}</a>
+								<p>{note.date}</p>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+      {/* end-of-card */}
+
+      {/* Announcements-card */}
+      <div className="announcement-container">
+				{announcements.map((announcement) => {
+					return (
+						<div className="announcement">
+							<div className="announcement-card">
+								<h2>{announcement.title}</h2>
+								<h4>{announcement.description}</h4>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+      {/* end-of-card */}
+		</div>
+	);
 }
 
 export default Student;
