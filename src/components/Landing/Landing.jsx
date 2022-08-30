fcbc7f94db19eb19185359a5fce4ec541db6f138
MissDine <enidnyatichi1212@gmail.com>
2022-08-30 10:01:16 +0300

    Feature edit

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Typed from "react-typed";
 import Card from "react-bootstrap/Card";
-<<<<<<< HEAD
-import "../Landing/Landing.css";
 import mainimg from "../../components/images/Events-cuate.png";
 import { Link } from "react-router-dom";
-=======
 import "./Landing.css"
->>>>>>> ae17858 (Style landing page)
 
 function Landing() {
-<<<<<<< HEAD
 	return (
 		<>
 			<Card className="typed " style={{ height: "100%", width: "100%" }}>
@@ -37,21 +32,6 @@ function Landing() {
 			</Card>
 		</>
 	);
-=======
-  return (
-    <>
-      <div className="land mx-auto" >
-      <Card className="typed text-center mx-auto" style={{ height: "70vh", width: "40vw", borderRadius: "15px" }}>
-        <Typed strings={["Ratiba"]} typeSpeed={150} backSpeed={150} loop style={{color: "#FF725E", paddingTop: "8%", fontSize: "2rem"}} />
-        {/* <h4 style={{ margin: "15px" }}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
-          consequatur.
-        </h4> */}
-      </Card>
-      </div>
-    </>
-  );
->>>>>>> ed6dc5b (Feature edit)
 }
 
 export default Landing;
