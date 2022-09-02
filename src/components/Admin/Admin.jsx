97b41f76ecf957228dddc0d32b261051c4650e6f
Robinson <robinadoro@gmail.com>
2022-09-02 08:06:44 +0300

    add create button

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 // import Button from "react-bootstrap/Button";
 // import Form from "react-bootstrap/Form";
 import "../Student/student.css";
@@ -28,10 +29,12 @@ function Admin() {
 
 
   return (
-    <div className="main-container">
+    <div className="main-container bg-aliceblue">
       <Nav />
       <User />
       <Search />
+      <div className="admincards d-flex ">
+
       <div className="card-contents">
         <div className="announcement-container">
           <h1 style={{ color: "black", fontSize: "2rem", fontWeight: "bold" }}>Announcements</h1>
@@ -40,11 +43,64 @@ function Admin() {
             {announcementToDisplay}
 
           </div>
+          <div className="cards">
+            
+          </div>
+          {/* <div className="btn d-flex">
+            <button className="announcebtn "> Create Announcement</button>
+            <button >Create Student</button>
+            <button></button>
+          </div> */}
 
         </div>
 
       </div>
+      <div className="cardi">
+        <div className="buttons">
+          <Link to="/adduser" >
+          <button className=""> Create Student</button>            
+            </Link>
+            <Link to="/announcement"> 
+          <button className="mr-4"> Create Announcement</button>
+            </Link>
+
+        </div>
+      <div className="row p-5 ">
+              <div className="card mb-3">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+              <div className="card mb-3 bg-aliceblue">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+              <div className="card mb-3">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+              <div className="card mb-3">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+              <div className="card mb-3">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+              <div className="card ">
+                <h2>Title</h2>
+                <h4>Lorem ipsum dolor sit amet.</h4>
+                <p>5, March 2022</p>
+              </div>
+            </div>
+      </div>
     </div>
+    </div>
+
 
 
   );
